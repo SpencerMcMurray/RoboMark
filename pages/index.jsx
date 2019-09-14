8a1ac51e17f1116244921ff942a7c72a5aa1ebb5
MohanVashist1 <mohan.vashist@mail.utoronto.ca>
2019-09-14 08:57:55 -0400

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TestButtons from "../components/TestButtons";
+
+const Index = props => {
+  return (
+    <React.Fragment>
+      <h1>Test</h1>
+      <TestButtons></TestButtons>
+    </React.Fragment>
+  );
+};
+
+export default Index;
